adeee13f4ffb4840eb7161805e9e9b57ba3527c1
andesonatasha <andesonatasha@gmail.com>
2026-02-26 09:37:09 +0300

    new codes

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+// Example of an object data type and how to access data
+let person = {
+    age: 24,
+    firstName:"Natasha",
+    lastName: "Andeso"
+};
+console.log(person["age"])
+console.log(person.firstName)
+
+let KeyName = "lastName";
+console.log(person[KeyName])
+
+// Array data Types
+const counties = ["Nairobi", "Mombasa", "Naivasha", "Machakos" ]
+console.log(counties[1]);
+console.log(counties[counties.length -1])
